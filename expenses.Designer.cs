2a44db1499d0c77e9a3e96230ab0f26389d2e9b4
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-03-08 13:00:10 +0500

    expenses code done

--- expenses.Designer.cs
+++ expenses.Designer.cs
@@ -28,6 +28,8 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label = new System.Windows.Forms.Label();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel8 = new System.Windows.Forms.Panel();
@@ -54,9 +56,11 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.panel13 = new System.Windows.Forms.Panel();
             this.panel12 = new System.Windows.Forms.Panel();
+            this.label11 = new System.Windows.Forms.Label();
             this.panel11 = new System.Windows.Forms.Panel();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -66,12 +70,16 @@
             this.label6 = new System.Windows.Forms.Label();
             this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.panel9 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.label16 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
             this.label14 = new System.Windows.Forms.Label();
             this.label17 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label19 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel8.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
@@ -92,6 +100,7 @@
             this.panel2.SuspendLayout();
             this.tableLayoutPanel2.SuspendLayout();
             this.panel9.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label
@@ -399,10 +408,14 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel2.BackColor = System.Drawing.Color.LightSeaGreen;
+            this.panel2.Controls.Add(this.label19);
+            this.panel2.Controls.Add(this.textBox6);
             this.panel2.Controls.Add(this.richTextBox1);
+            this.panel2.Controls.Add(this.button2);
             this.panel2.Controls.Add(this.button1);
             this.panel2.Controls.Add(this.panel13);
             this.panel2.Controls.Add(this.panel12);
+            this.panel2.Controls.Add(this.label11);
             this.panel2.Controls.Add(this.panel11);
             this.panel2.Controls.Add(this.textBox2);
             this.panel2.Controls.Add(this.textBox1);
@@ -424,16 +437,29 @@
             this.richTextBox1.Size = new System.Drawing.Size(132, 96);
             this.richTextBox1.TabIndex = 8;
             this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.RichTextBox1_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button2.Location = new System.Drawing.Point(190, 298);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "edit";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.button1.Location = new System.Drawing.Point(145, 298);
+            this.button1.Location = new System.Drawing.Point(94, 298);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 7;
             this.button1.Text = "save";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // panel13
             // 
@@ -455,12 +481,23 @@
             this.panel12.Size = new System.Drawing.Size(134, 2);
             this.panel12.TabIndex = 6;
             // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(-685, 15);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(141, 31);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "Expenses";
+            // 
             // panel11
             // 
             this.panel11.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel11.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.panel11.Location = new System.Drawing.Point(111, 115);
+            this.panel11.Location = new System.Drawing.Point(111, 130);
             this.panel11.Name = "panel11";
             this.panel11.Size = new System.Drawing.Size(134, 2);
             this.panel11.TabIndex = 6;
@@ -480,7 +517,7 @@
             this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.textBox1.BackColor = System.Drawing.Color.LightSeaGreen;
-            this.textBox1.Location = new System.Drawing.Point(111, 92);
+            this.textBox1.Location = new System.Drawing.Point(111, 107);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(132, 20);
             this.textBox1.TabIndex = 5;
@@ -512,7 +549,7 @@
             this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(35, 92);
+            this.label7.Location = new System.Drawing.Point(35, 107);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(23, 13);
             this.label7.TabIndex = 2;
@@ -555,16 +592,59 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel9.AutoSize = true;
             this.panel9.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel9.Controls.Add(this.button3);
+            this.panel9.Controls.Add(this.textBox7);
+            this.panel9.Controls.Add(this.dataGridView1);
             this.panel9.Controls.Add(this.label16);
             this.panel9.Controls.Add(this.label15);
             this.panel9.Controls.Add(this.label14);
             this.panel9.Controls.Add(this.label17);
             this.panel9.Controls.Add(this.label10);
-            this.panel9.Controls.Add(this.comboBox3);
             this.panel9.Location = new System.Drawing.Point(3, 3);
             this.panel9.Name = "panel9";
             this.panel9.Size = new System.Drawing.Size(690, 345);
             this.panel9.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(361, 21);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 11;
+            this.button3.Text = "Search";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(173, 22);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(189, 20);
+            this.textBox7.TabIndex = 10;
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.Turquoise;
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.MediumTurquoise;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.MediumAquamarine;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.Location = new System.Drawing.Point(17, 95);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(659, 250);
+            this.dataGridView1.TabIndex = 5;
             // 
             // label16
             // 
@@ -618,18 +698,21 @@
             this.label10.TabIndex = 0;
             this.label10.Text = "Expenses";
             // 
-            // comboBox3
+            // label19
             // 
-            this.comboBox3.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Items.AddRange(new object[] {
-            "abobakar",
-            "rafay",
-            "tayyab"});
-            this.comboBox3.Location = new System.Drawing.Point(208, 21);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(178, 21);
-            this.comboBox3.TabIndex = 0;
+            this.label19.AutoSize = true;
+            this.label19.Location = new System.Drawing.Point(42, 73);
+            this.label19.Name = "label19";
+            this.label19.Size = new System.Drawing.Size(16, 13);
+            this.label19.TabIndex = 20;
+            this.label19.Text = "Id";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(111, 66);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(132, 20);
+            this.textBox6.TabIndex = 19;
             // 
             // expenses
             // 
@@ -668,6 +751,7 @@
             this.tableLayoutPanel2.PerformLayout();
             this.panel9.ResumeLayout(false);
             this.panel9.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -716,7 +800,13 @@
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label17;
         private System.Windows.Forms.Label label10;
-        private System.Windows.Forms.ComboBox comboBox3;
         private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label19;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
--- expenses.cs
+++ expenses.cs
@@ -7,14 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace rafay_shop_project
 {
     public partial class expenses : Form
     {
+        SqlConnection connection;
+        SqlCommand command;
+        DataTable table = new DataTable();
+        string db_connection = @"Data Source=RANA-ABOBAKAR\SQLEXPRESS;Initial Catalog=rafays;Integrated Security=true";
+        bool edit_c = false;
         public expenses()
         {
             InitializeComponent();
+            this.connection = new SqlConnection(db_connection);
+            table.Columns.Add("Sr No");
+            table.Columns.Add("Title");
+            table.Columns.Add("Amount");
+            table.Columns.Add("Detail");
+           
+
+            dataGridView1.DataSource = table;
+            read_expenses();
         }
 
         private void Label_Click(object sender, EventArgs e)
@@ -73,6 +88,179 @@ namespace rafay_shop_project
             suppley suppley1 = new suppley();
             suppley1.Show();
             this.Hide();
+        }
+
+        private void RichTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void write_expenses()
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text) && !string.IsNullOrWhiteSpace(richTextBox1.Text))
+            {
+                try
+                {
+                    string sql = $"Insert into expenses(title,amount,detail)"
+                        + $"values('{textBox1.Text}','{textBox2.Text}','{richTextBox1.Text}')";
+                    connection.Open();
+                    command = new SqlCommand(sql, connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    adapter.InsertCommand = command;
+                    MessageBox.Show(adapter.InsertCommand.ExecuteNonQuery().ToString() + " New Record Added");
+
+                    connection.Close();
+                    textBox1.Text = textBox2.Text = richTextBox1.Text  = "";
+
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Something Missing!");
+            }
+        }
+        private void read_expenses()
+        {
+            try
+            {
+                SqlDataReader dataReader;
+                command = new SqlCommand("Select * from expenses", this.connection);
+                this.connection.Open();
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    DataRow row = this.table.NewRow();
+                    row["Sr No"] = dataReader.GetValue(0);
+                    row["Title"] = dataReader.GetValue(1);
+                    row["Amount"] = dataReader.GetValue(2);
+                    row["Detail"] = dataReader.GetValue(3);                  
+                    this.table.Rows.Add(row);
+                }
+                dataGridView1.Refresh();
+                this.connection.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+        private void update_expenses()
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text) && !string.IsNullOrWhiteSpace(textBox2.Text) && !string.IsNullOrWhiteSpace(richTextBox1.Text))
+            {
+                try
+                {
+                    int id = Convert.ToInt32(textBox7.Text);
+                    string sql = $"update expenses "
+                        + $"set title='{textBox1.Text}',amount='{textBox2.Text}',detail='{richTextBox1.Text}'  where id={id}";
+                    connection.Open();
+                    command = new SqlCommand(sql, connection);
+                    SqlDataAdapter adapter = new SqlDataAdapter();
+                    adapter.InsertCommand = command;
+                    MessageBox.Show(adapter.InsertCommand.ExecuteNonQuery().ToString() + "  Record update");
+                    connection.Close();
+                    dataGridView1.Refresh();
+                    table.Rows.Clear();
+                    read_expenses();
+                }
+                catch (Exception err)
+                {
+                    MessageBox.Show(err.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Something Missing!");
+            }
+        }
+        private void search_expenses()
+        {
+
+            try
+            {
+                table.Rows.Clear();
+                int id = Convert.ToInt32(textBox7.Text);
+                SqlDataReader dataReader;
+                command = new SqlCommand($"Select * from expenses where id ={id}", this.connection);
+                this.connection.Open();
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    DataRow row = this.table.NewRow();
+                    row["Sr No"] = dataReader.GetValue(0);
+                    row["Title"] = dataReader.GetValue(1);
+                    row["Amount"] = dataReader.GetValue(2);
+                    row["Detail"] = dataReader.GetValue(3);
+
+                    this.table.Rows.Add(row);
+                }
+                dataGridView1.Refresh();
+                this.connection.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            edit_c = true;
+            try
+            {
+                int id = Convert.ToInt32(textBox6.Text);
+                SqlDataReader dataReader;
+                command = new SqlCommand($"Select * from expenses where id={id}", this.connection);
+                this.connection.Open();
+                dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    textBox1.Text = (string)dataReader.GetValue(1);
+                    textBox2.Text = (string)dataReader.GetValue(2);
+                    richTextBox1.Text = (string)dataReader.GetValue(3);
+                  
+                }
+
+                this.connection.Close();
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
+            }
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+            if (edit_c == false)
+            {
+                write_expenses();
+                table.Rows.Clear();
+                read_expenses();
+            }
+            else
+            {
+                update_expenses();
+                edit_c = false;
+                textBox1.Text =  textBox2.Text = richTextBox1.Text = "";
+            }
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox7.Text))
+            {
+
+                search_expenses();
+            }
+            else
+            {
+                table.Rows.Clear();
+                read_expenses();
+            }
         }
     }
 }
