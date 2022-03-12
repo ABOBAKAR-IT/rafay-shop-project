69b139bf27f716de08782c1394dc511d093ca170
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2022-03-12 21:57:27 +0500

    add bill_form and report_form

---
+++ report.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace rafay_shop_project
+{
+    partial class report
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // report
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(998, 544);
+            this.Name = "report";
+            this.Text = "report";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
